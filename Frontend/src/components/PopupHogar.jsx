4142380bc28f038a836d472134273772fe89d4ec
Kaito <dilan.aranguiz2201@alumnos.ubiobio.cl>
2025-07-25 13:53:03 -0400

    Fix updatehogar

--- Frontend/src/components/PopupHogar.jsx
+++ Frontend/src/components/PopupHogar.jsx
@@ -5,11 +5,11 @@ import CloseIcon from '@assets/XIcon.svg';
 export default function PopupHogar({ show, setShow, data, action }) {
   const hogarData = data || {};
   
+const handleSubmit = (formData) => {
+  const nuevaDireccion = `${formData.tipo} ${formData.Direccion} #${formData.numero}`;
+  action(hogarData.id, nuevaDireccion);
+};
 
-  const handleSubmit = (formData) => {
-    action(hogarData.id, formData.nuevaDireccion);
-
-  };
 
 
   return (
@@ -20,21 +20,39 @@ export default function PopupHogar({ show, setShow, data, action }) {
             <button className="close" onClick={() => setShow(false)}>
               <img src={CloseIcon} />
             </button>
-            <Form
-              title="Actualizar hogar"
-              fields={[
-                {
-                  label: "Nueva dirección",
-                  name: "nuevaDireccion",
-                  fieldType: "textarea",
-                  placeholder: "Ej: Psje. rene olivares #1235",
-                  required: true,
-                  minLength: 5,
-                  maxLength: 150,
-                },
-              ]}
-              onSubmit={handleSubmit}
-              buttonText="Actualizar hogar"
+       <Form
+               title="Actualizar hogar"
+               fields={[
+                 {
+                   label: "Tipo",
+                   name: "tipo",
+                   fieldType: 'select',
+                   options: [
+                     { value: "Psj.", label: "Psj." },
+                     { value: "Av.", label: "Av." }
+                   ],
+                   required: true,
+                   maxLength: 255,
+                 },
+                 {
+                   label: "Dirección",
+                   name: "Direccion",
+                   fieldType: 'input',
+                   type: "text",
+                   required: true,
+                   maxLength: 50,
+                 },
+                 {
+                   label: "Número #",
+                   name: "numero",
+                   fieldType: 'input',
+                   type: "number",
+                   required: true,
+                   min: 1,
+                 }
+               ]}
+               buttonText="Actualizar hogar"
+               onSubmit={handleSubmit}
               backgroundColor="#fff"
             />
           </div>
